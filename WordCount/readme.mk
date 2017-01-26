36d789014cc5dcc3c171eb93648d3be19bcf01de
cloudspurs <443051430@qq.com>
2017-01-27 00:02:26 +0800

    WordCount program

---
+++ WordCount/readme.mk
@@ -0,0 +1,7 @@
+Flex program has 3 parts.
+First part is delaration and options.
+Second part is Pattern and Action.
+Third part is c code, it will be copy to c program that flex generate.
+
+yytect is point to the string that is processing.
+
